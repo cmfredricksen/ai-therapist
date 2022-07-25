3b92793a9d706ed7ed39df3625177e3ca30492df
Chris Fredricksen <cmfredricksen@gmail.com>
2022-07-25 17:58:41 -0500

    fixed img paths

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,11 +3,11 @@
 <main>
 
     <img src="/church-ruins.jpg" alt="AI Art">
-    <img src="/static/trees-from-rocks.jpg" alt="AI Art">
+    <img src="/trees-from-rocks.jpg" alt="AI Art">
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat deleniti omnis assumenda officia sapiente facilis molestiae ullam fugit accusamus quis vero tempore culpa, modi minus quidem reprehenderit eum libero perspiciatis necessitatibus! Illo unde numquam, provident dolores aliquid officia et ipsa reiciendis delectus reprehenderit cumque dolore voluptate cum dignissimos earum iusto harum assumenda quidem exercitationem dolor, vero excepturi aliquam dicta? Odio veritatis consectetur ipsa, corrupti obcaecati voluptatibus magni ut accusamus iusto ab esse aperiam nostrum voluptatem?</p>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad sapiente quam sed commodi! Nesciunt aliquid vitae praesentium eaque sequi autem in et, iusto architecto itaque, facilis omnis. Voluptatum facere doloremque porro, saepe at suscipit aliquam? Saepe reprehenderit voluptatibus pariatur nostrum voluptate, est ducimus repudiandae? Voluptate debitis reprehenderit delectus iusto odit culpa nam! Aperiam distinctio qui ipsa reiciendis aspernatur dolorem! Nisi temporibus aperiam corrupti nostrum beatae, fugit deleniti laudantium dignissimos vel optio doloremque molestias. Fuga voluptatum, minus, dicta, nostrum iusto a rem ab asperiores dolores omnis aperiam iste dolorem eum. Accusantium eos placeat et minus accusamus adipisci illo cumque nisi commodi!</p>
-    <img src="/static/water-lilies.jpg" alt="AI Art">
-    <img src="/static/forest-path.png" alt="AI Art">
+    <img src="/water-lilies.jpg" alt="AI Art">
+    <img src="/forest-path.png" alt="AI Art">
 </main>
 
 <style>
